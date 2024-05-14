cf37faf307e8aa05b418d5c4372b134c5d372450
kritigupta45 <kritigupta2024@gmail.com>
2024-05-14 15:17:40 +0530

    implement home ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,31 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotlaBalanceBox from '@/components/TotlaBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {
+    firstName: 'Kriti'
+  };
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotlaBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
