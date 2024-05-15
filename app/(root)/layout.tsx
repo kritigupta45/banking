904b40da769af4fae6ee76ff65216a7e39048b31
kritigupta45 <kritigupta2024@gmail.com>
2024-05-15 14:33:29 +0530

    Implement auth, appwrite and sentry

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,18 +1,17 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
-// import { getLoggedInUser } from "@/lib/actions/user.actions";
+import { getLoggedInUser } from "@/lib/actions/user.action";
 import Image from "next/image";
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = {firstName: 'Kriti', lastName:'Gupta'};
-  // const loggedIn = await getLoggedInUser();
+  const loggedIn = await getLoggedInUser();
 
-  // if(!loggedIn) redirect('/sign-in')
+  if(!loggedIn) redirect('/sign-in')
 
   return (
     <main className="flex h-screen w-full font-inter">
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,11 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar';
 import TotlaBalanceBox from '@/components/TotlaBalanceBox';
+import { getLoggedInUser } from '@/lib/actions/user.action';
 import React from 'react'
 
-const Home = () => {
-  const loggedIn = {
-    firstName: 'Kriti', lastName:'Gupta', email: 'contact@myid.in'
-  };
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
 
   return (
     <section className='home'>
@@ -15,7 +14,7 @@ const Home = () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.name || 'Guest'}
             subtext="Access and manage your account and transactions efficiently."
           />
 
